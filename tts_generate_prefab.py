7f1380cbac0152546bdd7a53df92f2455d4f32e8
New Carrot Games <newcarrotgames@gmail.com>
2020-03-17 08:54:44 -0400

    save generated prefab image correctly

--- tts_generate_prefab.py
+++ tts_generate_prefab.py
@@ -10,58 +10,70 @@ from tensorflow.keras import layers
 import time
 from read_training_data import read_training_data
 from IPython import display
+from pprint import pprint
+
 
 def make_generator_model():
-        model = tf.keras.Sequential()
-        model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
-        model.add(layers.BatchNormalization())
-        model.add(layers.LeakyReLU())
-        model.add(layers.Reshape((7, 7, 256)))
-        assert model.output_shape == (None, 7, 7, 256) # Note: None is the batch size
-        model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
-        assert model.output_shape == (None, 7, 7, 128)
-        model.add(layers.BatchNormalization())
-        model.add(layers.LeakyReLU())
-        model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-        assert model.output_shape == (None, 14, 14, 64)
-        model.add(layers.BatchNormalization())
-        model.add(layers.LeakyReLU())
-        model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
-        assert model.output_shape == (None, 28, 28, 1)
-        return model
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    model.add(layers.Reshape((7, 7, 256)))
+    # Note: None is the batch size
+    assert model.output_shape == (None, 7, 7, 256)
+    model.add(layers.Conv2DTranspose(
+        128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 7, 7, 128)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    model.add(layers.Conv2DTranspose(
+        64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 14, 14, 64)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2),
+                                     padding='same', use_bias=False, activation='tanh'))
+    assert model.output_shape == (None, 28, 28, 1)
+    return model
+
 
 def make_discriminator_model():
-        model = tf.keras.Sequential()
-        model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same', input_shape=[28, 28, 1]))
-        model.add(layers.LeakyReLU())
-        model.add(layers.Dropout(0.3))
-        model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
-        model.add(layers.LeakyReLU())
-        model.add(layers.Dropout(0.3))
-        model.add(layers.Flatten())
-        model.add(layers.Dense(1))
-        return model
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2),
+                            padding='same', input_shape=[28, 28, 1]))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+    return model
+
 
 def array_to_image(data, filename):
     img = PIL.Image.fromarray(data, '1')
     img.save(filename)
 
+
 def generate_and_save_images(model, test_input):
     predictions = model(test_input, training=False)
-    fig = plt.figure(figsize=(1,1))
+    fig = plt.figure(figsize=(1, 1))
     print("predictions shape: {}", predictions.shape)
+    prediction = (predictions[0, :, :, 0] * 127.5 + 127.5).numpy()
+    np.savetxt("prediction.csv", prediction, delimiter=",")
     for i in range(predictions.shape[0]):
-            plt.subplot(1, 1, i+1)
-            plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
-            plt.axis('off')
-    plt.savefig('generated_prefab.png')
-    plt.show()
+        plt.subplot(1, 1, i+1)
+        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.imsave('generated_prefab_{}.png'.format(i), predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.close('all')
+
 
 print("tensorflow version: {}".format(tf.__version__))
 generator = make_generator_model()
 discriminator = make_discriminator_model()
 noise = tf.random.normal([1, 100])
-generated_image = generator(noise, training=False) 
+generated_image = generator(noise, training=False)
 generator_optimizer = tf.keras.optimizers.Adam(1e-4)
 discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
 checkpoint_dir = './training_checkpoints'
